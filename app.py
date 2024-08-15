8c1b42845d6d6ac8eafa88f3d0d7d0bee5b348d8
ZHAO XIAOTIAN <73349073+Wayen-319@users.noreply.github.com>
2024-08-15 21:31:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request
 app=Flask(__name__)
 
 @app.route('/', methods=['get','post'])
-def DApp():
-    return render_template('DApp.html')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
